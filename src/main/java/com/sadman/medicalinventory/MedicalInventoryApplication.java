ee5c8bfe030a8949fdd31841550c95c8960db665
Sadman <imran110219@gmail.com>
2021-01-11 12:43:10 +0600

    txt

---
+++ src/main/java/com/sadman/medicalinventory/MedicalInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.sadman.medicalinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sadman/medicalinventory/MedicalInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadman.medicalinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
