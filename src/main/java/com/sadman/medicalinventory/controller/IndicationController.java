447464e77224242c9b774a77da29584607be9097
Sadman Sobhan <imran110219@gmail.com>
2021-03-08 01:26:10 +0600

    frontend add

--- src/main/java/com/sadman/medicalinventory/controller/IndicationController.java
+++ src/main/java/com/sadman/medicalinventory/controller/IndicationController.java
@@ -1,6 +1,10 @@
 package com.sadman.medicalinventory.controller;
 
+import com.sadman.medicalinventory.model.Indication;
 import com.sadman.medicalinventory.model.User;
+import com.sadman.medicalinventory.service.IndicationService;
+import com.sadman.medicalinventory.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,14 @@ import java.util.List;
 
 @Controller
 public class IndicationController {
+    @Autowired
+    private IndicationService service;
 
     @RequestMapping(value = "/indications")
     public String getAllIndications(Model model) {
-        return "indication-list";
+        List<Indication> list = service.getAllIndications();
+        model.addAttribute("indications", list);
+//        return "indication-list";
+        return "indications";
     }
 }
