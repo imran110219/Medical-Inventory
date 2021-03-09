f2d296a4b5bc312b4145e24d3d65b6ed912d57ca
Sadman Sobhan <imran110219@gmail.com>
2021-03-09 23:52:11 +0600

    frontend add

--- src/main/java/com/sadman/medicalinventory/controller/BrandController.java
+++ src/main/java/com/sadman/medicalinventory/controller/BrandController.java
@@ -1,7 +1,9 @@
 package com.sadman.medicalinventory.controller;
 
 import com.sadman.medicalinventory.model.Brand;
+import com.sadman.medicalinventory.model.DosageForm;
 import com.sadman.medicalinventory.service.BrandService;
+import com.sadman.medicalinventory.service.DosageFormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +16,15 @@ public class BrandController {
     @Autowired
     private BrandService service;
 
+    @Autowired
+    private DosageFormService dosageFormService;
 
     @RequestMapping(value = "/brands")
     public String getAllUsers(Model model) {
-        List<Brand> list = service.getAllBrands();
-        model.addAttribute("brands", list);
+        List<Brand> brandList = service.getAllBrands();
+        List<DosageForm> dosageFormList = dosageFormService.getAllDosageForms();
+        model.addAttribute("dosageforms", dosageFormList);
+        model.addAttribute("brands", brandList);
         return "brand-list";
     }
 
