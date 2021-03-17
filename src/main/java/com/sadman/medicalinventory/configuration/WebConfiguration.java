b5de21e6814d6d12396974a81466450bbe5e2807
Sadman Sobhan <imran110219@gmail.com>
2021-03-17 23:53:00 +0600

    ajax dialogue error

--- src/main/java/com/sadman/medicalinventory/configuration/WebConfiguration.java
+++ src/main/java/com/sadman/medicalinventory/configuration/WebConfiguration.java
@@ -1,13 +1,30 @@
 package com.sadman.medicalinventory.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @EnableWebMvc
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+//    @Bean
+//    public ClassLoaderTemplateResolver yourTemplateResolver() {
+//        ClassLoaderTemplateResolver configurer = new ClassLoaderTemplateResolver();
+//        configurer.setPrefix("templates/");
+//        configurer.setSuffix(".html");
+//        configurer.setTemplateMode(TemplateMode.HTML);
+//        configurer.setCharacterEncoding("UTF-8");
+//        configurer.setOrder(0);
+//        configurer.setCacheable(false);
+//        configurer.setCheckExistence(true);
+//        return configurer;
+//    }
+
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
             "classpath:/META-INF/resources/",
             "classpath:/resources/",
